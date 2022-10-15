a031006ffbff6eca3c7efe18e5d88c3b58078e48
Pratyay Dhond <dhondpratyay@gmail.com>
2022-10-15 10:20:06 +0530

    Removed isFavourite field from all quotes in firestore and archieved function that was used for doing the same
    archieved at AddQuote.js -> Line no. 46

--- src/Components/Pages/AddQuote.js
+++ src/Components/Pages/AddQuote.js
@@ -37,27 +37,26 @@ async function getQuotes ()  {
                 });
         }).finally(()=> { 
             setQuotes(firebaseQuotes)
-            updateQuotesOnFirebase(firebaseQuotes);
         })
     }catch(e){
         console.log(e);   
     }
 }   
 
-async function updateQuotesOnFirebase(firebaseQuotes){
-    console.log(firebaseQuotes);
-    firebaseQuotes.forEach(async quote => {
-        await firebase.firestore().collection("quotes").doc(quote.id).set({
-            userID: quote.userID,
-            quote: quote.quote,
-            author: quote.author,
-            source: quote.source,
-            rating: quote.rating,
-            time: quote.time,
-            updateTime: quote.updateTime,
-        })
-    })
-}
+// async function updateQuotesOnFirebase(firebaseQuotes){
+//     console.log(firebaseQuotes);
+//     firebaseQuotes.forEach(async quote => {
+//         await firebase.firestore().collection("quotes").doc(quote.id).set({
+//             userID: quote.userID,
+//             quote: quote.quote,
+//             author: quote.author,
+//             source: quote.source,
+//             rating: quote.rating,
+//             time: quote.time,
+//             updateTime: quote.updateTime,
+//         })
+//     })
+// }
 
 
 
