9c66825bfdca7174b876f4331e5fa668a69646fc
Pratyay Dhond <dhondpratyay@gmail.com>
2022-10-28 10:03:45 +0530

    Added CSS for Android Portrait view for FullPageQuoteCard

--- src/Components/SearchPage/SearchPageQuoteCardFullPageView.js
+++ src/Components/SearchPage/SearchPageQuoteCardFullPageView.js
@@ -57,10 +57,6 @@ const SearchPageQuoteCardFullPageView = ({userID, deleteQuote, setDeleteQuote, s
 
                             }}/>
 
-                    <div className='SearchPageQuoteCardFullPageView-TimeStamp'>
-                    {time.day}{time.ordinals} {time.monthString}, {time.year}
-                    </div>
-                 
                     <div className='SearchPageQuoteCardFullPageView-Quote'>
                         <span className='SearchPageQuoteCardFullPageView-DoubleQuotes'>“</span>
                             {quote.quote}
@@ -74,6 +70,12 @@ const SearchPageQuoteCardFullPageView = ({userID, deleteQuote, setDeleteQuote, s
                     <div className="SearchPageQuoteCardFullPageView-Source">
                         {source}
                     </div>
+
+                    
+                    <div className='SearchPageQuoteCardFullPageView-TimeStamp'>
+                    {time.day}{time.ordinals} {time.monthString}, {time.year}
+                    </div>
+                 
 
                     <div className='SearchPageQuoteCardFullPageView-favourite'>
                         {
