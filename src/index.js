035f43f45d6ca8bd6ce79ba4b5d9f9f75af77454
Pratyay Dhond <dhondpratyay@gmail.com>
2022-10-29 12:24:05 +0530

    Added Environment Variable files

--- src/index.js
+++ src/index.js
@@ -7,21 +7,11 @@ import reportWebVitals from './reportWebVitals';
 
 import firebase  from "firebase/compat/app";
 import { getAnalytics } from "firebase/analytics";
-
+import firebaseConfig from './env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBidTZirEbucXDx8Xb9VRm8Y2k0rQRdvcM",
-  authDomain: "quotation-react.firebaseapp.com",
-  projectId: "quotation-react",
-  storageBucket: "quotation-react.appspot.com",
-  messagingSenderId: "564855116435",
-  appId: "1:564855116435:web:79e2229f6b247bf4c63366",
-  measurementId: "G-P7HV5JL86X"
-};
+
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
